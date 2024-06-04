b87dccdf8ea172cbb807b0e2d530095a2aacc844
Contaro <b.deni.goperty@yandex.ru>
2024-06-04 22:53:58 +0300

    add submodule pull down

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include "adder.h"
+#include "GLFW/glfw3.h"
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    cout << mearlymath::add(2, 3) << endl;
+    cout << mearlymath::add(40, 2) << endl;
+
+    GLFWwindow* window;
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
